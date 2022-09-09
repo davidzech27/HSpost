357d79ebc6b74eb52513eb110b630e36536eee6f
David Z <davidzech27@gmail.com>
2022-09-09 10:59:32 -0700

    Create monorepo with client and server

---
+++ README.md
@@ -0,0 +1,1 @@
+## A social network platform for high school students and their classmates to connect
---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppProps } from "next/app"
+
+const App = ({ Component }: AppProps) => {
+	return <Component />
+}
+
+export default App
