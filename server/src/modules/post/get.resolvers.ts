f4521dd5e93430bd58145d2fb10738326fe742d3
David Z <davidzech27@gmail.com>
2022-10-21 02:38:44 -0700

    Rename query

--- server/src/modules/post/get.resolvers.ts
+++ server/src/modules/post/get.resolvers.ts
@@ -2,7 +2,7 @@ import Resolvers from "types/resolvers"
 
 const resolvers: Resolvers = {
 	Query: {
-		posts: async (_, { id }, { db }) => {
+		getPost: async (_, { id }, { db }) => {
 			return await db.post.findUnique({
 				where: {
 					id
