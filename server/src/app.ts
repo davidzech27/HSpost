5a0c2e2a2dd7e9d8e03f3843f4d6154aadd9bf4a
David Z <davidzech27@gmail.com>
2022-10-21 23:27:24 -0700

    Logging when server starts

--- server/src/app.ts
+++ server/src/app.ts
@@ -47,7 +47,9 @@ const startServer = () => {
 
 	app.use(/\//, server)
 
-	app.listen({ port: process.env.PORT })
+	app.listen({ port: process.env.PORT }, () =>
+		console.log(`Server running on port ${process.env.PORT}`)
+	)
 }
 
 startServer()
