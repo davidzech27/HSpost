c1a61b31ef69f1b020f298bc79263521758d8a35
David Z <davidzech27@gmail.com>
2022-10-23 00:53:50 -0700

    Make feed query take skip argument instead of afterId

--- server/src/modules/feed/feed.resolvers.ts
+++ server/src/modules/feed/feed.resolvers.ts
@@ -2,26 +2,14 @@ import Resolvers from "types/resolvers"
 
 const resolvers: Resolvers = {
 	Query: {
-		feed: async (_, { take = 20, afterId }, { db }) => {
-			if (!afterId) {
-				return await db.post.findMany({
-					orderBy: {
-						commentCount: "desc"
-					},
-					take
-				})
-			} else {
-				return await db.post.findMany({
-					orderBy: {
-						commentCount: "desc"
-					},
-					cursor: {
-						id: afterId
-					},
-					skip: 1,
-					take
-				})
-			}
+		feed: async (_, { take = 20, skip = 0 }, { db }) => {
+			return await db.post.findMany({
+				orderBy: {
+					commentCount: "desc"
+				},
+				take,
+				skip
+			})
 		}
 	}
 }
