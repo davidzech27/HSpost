cd77a9ae263d0374bdfc679bf41cc2de8ee59a5d
David Z <davidzech27@gmail.com>
2022-10-25 23:03:43 -0700

    Add modal to _app.tsx and meta tag with referrerpolicy

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,7 +1,29 @@
+import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
+import Head from "next/head"
+import queryClient from "lib/queryClient"
+import api from "lib/api"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import useCallOnce from "hooks/useCallOnce"
+import useModalStore from "stores/modalStore"
 
 const App = ({ Component }: AppProps) => {
-	return <Component />
+	const modal = useModalStore((state) => state.modal)
+
+	return (
+		<>
+			<Head>
+				<meta name="referrer" content="no-referrer" />
+			</Head>
+
+			<QueryClientProvider client={queryClient}>
+				<Component />
+				{/* <ReactQueryDevtools /> */}
+			</QueryClientProvider>
+			{modal}
+		</>
+	)
 }
 
 export default App
